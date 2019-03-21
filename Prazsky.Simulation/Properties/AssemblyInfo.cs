9be29fea8ff1582ec0aa116bc5c8205697ac30c0
AntoninPrazsky <33231531+AntoninPrazsky@users.noreply.github.com>
2019-03-21 23:20:34 +0100

    Update version number

--- Prazsky.Simulation/Properties/AssemblyInfo.cs
+++ Prazsky.Simulation/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Obecné informace o sestavení se řídí přes následující 
-// sadu atributů. Změnou hodnot těchto atributů se upraví informace
-// přidružené k sestavení.
 [assembly: AssemblyTitle("Prazsky.Simulation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,23 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
-// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
-// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
 [assembly: ComVisible(false)]
 
-// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
 [assembly: Guid("86d904c7-8653-411c-a9ce-7e69c3a49868")]
 
-// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
-//
-//      Hlavní verze
-//      Podverze
-//      Číslo sestavení
-//      Revize
-//
-// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize pomocí *, jak je vidět níže:
-// pomocí zástupného znaku * takto:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
